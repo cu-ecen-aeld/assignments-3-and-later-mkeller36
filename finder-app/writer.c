bd127fdd95377ca181606ea50530858548224d4e
mjkeller <michaelkeller1997@gmail.com>
2024-08-06 19:59:06 -0400

    completed finder and logging

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -8,9 +8,30 @@ openlog(NULL,0,LOG_USER);
     if(argc == 3){
         syslog(LOG_DEBUG,"Made it into if statement, Correct number of arguments.");
         FILE *fileptr;
+        int retval;
         fileptr = fopen(argv[1], "w");
-        fputs(argv[2], fileptr);
-        fclose(fileptr);
+        if(fileptr!=NULL){
+            syslog(LOG_DEBUG,"Successfully opened file");
+        }
+        else{
+            syslog(LOG_ERR, "Could not open file");
+        }
+
+        retval = fputs(argv[2], fileptr);
+        if(retval != -1){
+            syslog(LOG_DEBUG, "Successfully wrote %s to file %s", argv[1], argv[2]);
+        }
+        else{
+            syslog(LOG_ERR, "Could not write to file");
+        }
+
+        retval = fclose(fileptr);
+        if(retval != -1){
+            syslog(LOG_DEBUG, "Successfully closed file");
+        }
+        else{
+            syslog(LOG_ERR, "Could not close file");
+        }
     }
     else{
         syslog(LOG_ERR, "Invalid number of arguments: %d", argc);
